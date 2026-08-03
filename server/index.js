83a5345c33e985bef23badc800bcfeb4bd6e8792
Aditya Nikam <nikamaditya7720@gmail.com>
2026-08-03 19:56:19 +0530

    fix: remove duplicate cors variable declaration

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cors = require("cors");
-
 // Allow all origins
 app.use(cors());
 const bcrypt = require("bcryptjs");
